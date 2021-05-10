ad4ce972a2f786ce88f7b13c31c27fd51cce37a2
DianQK <dianqk@icloud.com>
2021-05-10 15:28:30 +0800

    Initial Commit

---
+++ LTOGlobalMachineOutliner/LTOGlobalMachineOutlinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTOGlobalMachineOutlinerApp.swift
+//  LTOGlobalMachineOutliner
+//
+//  Created by 呀哈哈 on 2021/5/10.
+//
+
+import SwiftUI
+
+@main
+struct LTOGlobalMachineOutlinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
